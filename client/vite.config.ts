1dc5a83f008c3db2ea2ba6538329044a639172bb
Dhruv <dhruvprajapati1622004@gmail.com>
2026-05-06 08:37:23 +0530

    add: env url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['scms.clg'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
